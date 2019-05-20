7062719167f239c88076297429fed5f37a9d0420
bolf <bolf@li.ru>
2019-05-20 23:56:59 +0300

    + routes

--- routes/logger.js
+++ routes/logger.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const dateFormat = require("dateformat");
 
 const logSchema = new mongoose.Schema({
   eventType: { type: String, required: true, index: true },
@@ -9,10 +10,37 @@ const logSchema = new mongoose.Schema({
   comment: { type: String, required: false }
 });
 
-router.get("/", (req, res) => {
+const LogModel = mongoose.model("Log", logSchema);
+
+router.get("/", async (req, res) => {
+  if (!(req.body.eventType && req.body.eventDate)) {
+    return res
+      .status(400)
+      .send(
+        "для получения данных укажите отборы: eventDate (сравнение на больше или равно), eventType (сравнение на равно)"
+      );
+  }
+
   try {
+    const logs = await LogModel.find({
+      eventType: req.body.eventType,
+      eventDate: {
+        $gte: Date.parse(dateFormat(req.body.eventDate, "isoUtcDateTime"))
+      }
+    }).sort("eventDate");
+    res.send(logs);
   } catch (e) {
-    res.status(500).send("что-то пошло не так..");
+    res.status(400).send("что-то пошло не так..");
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    var log = new LogModel(req.body);
+    log = await log.save();
+    res.send(log._id);
+  } catch (e) {
+    res.status(400).send("что-то пошло не так..");
   }
 });
 
